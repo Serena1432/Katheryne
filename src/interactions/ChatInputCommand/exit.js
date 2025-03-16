98e2d756de8da45dd811f23ac02c39a1fe1d5da0
Tora Phan Nam <admin@s1432.org>
2025-03-16 13:14:00 +0700

    Add exit slash command

---
+++ src/interactions/ChatInputCommand/exit.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder, ChatInputCommandInteraction } = require("discord.js");
+const Language = require("../../classes/Language");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("exit")
+    .setDescription(Language.strings.exit.description),
+    config: {
+        nodm: true,
+        memberPermissions: [],
+        botPermissions: [],
+        ownerOnly: false
+    },
+    /**
+     * 
+     * @param {Client} client 
+     * @param {ChatInputCommandInteraction} interaction 
+     * @returns 
+     */
+    run: async (client, interaction) => {
+        await interaction.deferReply();
+        await client.commands.get("exit").run(client, interaction, []);
+    }
+}
