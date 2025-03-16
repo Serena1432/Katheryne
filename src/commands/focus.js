d668c8cd8ae5ff154b0a0c21e00587bef3511a80
Tora Phan Nam <admin@s1432.org>
2025-03-16 14:07:57 +0700

    Add the focus command

---
+++ src/commands/focus.js
@@ -0,0 +1,37 @@
+const { Client, Message } = require("discord.js");
+const Language = require("../classes/Language");
+const Katheryne = require("../classes/Katheryne");
+const Computer = require("../classes/Computer");
+const Steam = require("../classes/Steam");
+const WhitelistedApps = require("../classes/WhitelistedApps").WhitelistedAppManager;
+
+module.exports.config = {
+    name: "focus",
+    usage: "focus",
+    description: Language.strings.focus.description,
+    nodm: true,
+    memberPermissions: [],
+    botPermissions: [],
+    ownerOnly: false
+}
+
+/**
+ * 
+ * @param {Client} client 
+ * @param {Message} message 
+ * @param {string[]} args 
+ */
+module.exports.run = async function(client, message, args) {
+    var runningApps = await WhitelistedApps.running();
+    if (!runningApps.length) return Katheryne.reply(message, {content: Language.strings.noRunningPermissions});
+    var app = runningApps[0];
+    var msg = await Katheryne.reply(message, {content: Language.strings.logs.preparing});
+    try {
+        Computer.focus(app.name);
+        Katheryne.editMessage(msg, {content: Language.strings.focus.success.format(app.name)});
+    }
+    catch (err) {
+        console.error(err);
+        Katheryne.addLog(msg, err.stack);
+    }
+}
